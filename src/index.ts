aecf3818b802096308b24bdf45a000a6c9a24fa3
cploujoux <ch.ploujoux@gmail.com>
2025-09-30 10:56:19 +0200

    Remove non necessary code

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,6 @@ async function main() {
   console.info("Booting up...");
   const app = Fastify();
 
-  app.addHook("onRequest", async (request, reply) => {
-    console.info(`${request.method} ${request.url}`);
-  });
-
   // You can start developing your agent from this endpoint
   // You are not limited to this endpoint, you can create as many as you want
   app.post<{ Body: RequestBody }>("/", async (request, reply) => {
@@ -21,13 +17,9 @@ async function main() {
 
   const port = parseInt(process.env.BL_SERVER_PORT || "80");
   const host = process.env.BL_SERVER_HOST || "0.0.0.0";
-  try {
-    await app.listen({ port, host });
-    console.info(`Server is running on port ${host}:${port}`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+
+  await app.listen({ port, host });
+  console.info(`Server is running on port ${host}:${port}`);
 }
 
 main().catch(console.error);
